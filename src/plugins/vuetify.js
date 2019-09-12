90e633fd935444800f8aca067345075ff1bc4eff
Sameh <s.gamal@solutionplus.net>
2019-09-12 14:00:34 +0200

    Sync

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -10,9 +10,9 @@ export default new Vuetify({
   theme: {
     themes: {
       light: {
+        accent: "#82B1FF",
         primary: "#1976D2",
         secondary: "#424242",
-        accent: "#82B1FF",
         error: "#FF5252",
         info: "#2196F3",
         success: "#4CAF50",
