d8a20798ea79019fa4135dd2aec5446dd0413d45
Sameh <s.gamal@solutionplus.net>
2019-09-12 14:42:51 +0200

    Commit test 3

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -10,7 +10,7 @@ export default new Vuetify({
   theme: {
     themes: {
       light: {
-        accent: "#82B1FF",
+        accent: "#82B1F2",
         primary: "#1976D2",
         secondary: "#424242",
         error: "#FF5252",
