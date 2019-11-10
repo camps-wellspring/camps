0ee86a86cac79fc822de8f3d089c347a2cbea15d
ahmedismail1993 <ahmed_ali_1993@yahoo.com>
2019-11-10 16:56:13 +0200

    merge

--- src/views/works/action.vue
+++ src/views/works/action.vue
@@ -40,7 +40,7 @@
               </form-group>
             </v-col>
             <v-col cols="12" md="6">
-              <form-group name="priority">
+              <form-group name="description">
                 <template slot-scope="{ attrs }">
                   <v-textarea
                     v-bind="attrs"
