f9aaf23acf65bbfa6edc3a4fd2c1a3ee36d9411c
Ajemy <a.gamal@solutionplus.net>
2019-11-25 09:29:35 +0200

    merge

--- src/helpers/deepFormData.js
+++ src/helpers/deepFormData.js
@@ -20,7 +20,11 @@ export const deepFormData = form => {
       // if the property is an array
       if (form[el] instanceof Array) {
         // if THAT array is an array of objects (has at least one object)
-        if (form[el].some(val => typeof val === "object" && !(val instanceof File))) {
+        if (
+          form[el].some(
+            val => typeof val === "object" && !(val instanceof File)
+          )
+        ) {
           // flattening each object in that array
           form[el].forEach((obj, i) => {
             for (const prop in obj) {
