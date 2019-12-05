7c0cabf0e2e058cc26a1b592e05479f8be446f5d
Ahmed Mabrouk <a.mabrouk@solutionplus.net>
2019-12-05 12:11:44 +0200

    remove dist

--- dist/js/chunk-2d0c7711.2d9a6672.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c7711"],{5143:function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("h1",[n._v("401")])},u=[],a={name:"Page401"},l=a,r=t("2877"),s=Object(r["a"])(l,c,u,!1,null,null,null);e["default"]=s.exports}}]);
-//# sourceMappingURL=chunk-2d0c7711.2d9a6672.js.map
