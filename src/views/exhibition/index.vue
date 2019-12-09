c3f22b44f2f169b5cc8741371d661ceecaddbb73
Sameh <s.gamal@solutionplus.net>
2019-12-09 13:56:34 +0200

    Information center gallery

--- src/views/exhibition/index.vue
+++ src/views/exhibition/index.vue
@@ -10,13 +10,27 @@
       @ButtonClicked="savePhotos"
     />
 
+    <div class="d-flex align-baseline justify-center">
+      <span class="mx-3" v-text="$t('label.information_center')" />
+      <v-switch
+        v-model="type"
+        inset
+        hide-details
+        class="mx-4"
+        true-value="main_gallery"
+        false-value="information_center"
+        :loading="loading.fetch"
+      />
+      <span v-text="$t('label.honorary_exhibition')" />
+    </div>
+
     <v-container>
       <multi-image-upload
         ref="imgUpload"
         :maxSize="4"
-        :imgsUrl="items"
+        :imgsUrl="photos"
         @fileSelected="newPhotos = $event"
-        @handle_delete_image="items.splice($event, 1)"
+        @handle_delete_image="photos.splice($event, 1)"
       />
     </v-container>
   </div>
@@ -30,9 +44,10 @@ export default {
 
   data() {
     return {
-      items: [],
-      reset: false,
+      photos: [],
       newPhotos: [],
+
+      type: "main_gallery",
 
       loading: {
         fetch: false,
@@ -41,21 +56,29 @@ export default {
     };
   },
 
-  created() {
-    this.fetchItems();
+  watch: {
+    type: {
+      immediate: true,
+      handler() {
+        this.fetchItems();
+      }
+    }
   },
 
   methods: {
     fetchItems() {
       this.loading.fetch = true;
-      IndexData({ reqName: "photos-galleries", query: { pagination: "all" } })
+      const query = { pagination: "all" };
+      this.type === "information_center" && (query.information_center = true);
+      IndexData({ reqName: "photos-galleries", query })
         .then(res => {
-          this.items = res.data.data;
+          this.photos = res.data.data;
           this.newPhotos = [];
           this.$refs.imgUpload.resetImageFile();
           this.loading.fetch = false;
         })
         .catch(() => {
+          this.type === "exhibition" ? "information_center" : "exhibition";
           this.loading.fetch = false;
         });
     },
@@ -64,6 +87,7 @@ export default {
       this.loading.submit = true;
       let data = new FormData();
       this.newPhotos.forEach((photo, i) => data.append(`photos[${i}]`, photo));
+      data.append("title", this.type);
       StoreData({ reqName: "photos-galleries", data })
         .then(() => {
           this.loading.submit = false;
