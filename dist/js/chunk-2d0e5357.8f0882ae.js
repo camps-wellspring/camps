6e1d06646d6d1eb612e24a74a3a59d1b1f174edd
Ahmed Mabrouk <a.mabrouk@solutionplus.net>
2020-01-22 09:58:46 +0200

    build for production

---
+++ dist/js/chunk-2d0e5357.8f0882ae.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e5357"],{9406:function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},c=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("DashBoard")])])}],l={name:"DashBoardPage",methods:{handleClick(){console.log("clicked")}}},o=l,r=t("2877"),s=Object(r["a"])(o,a,c,!1,null,null,null);n["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0e5357.8f0882ae.js.map
