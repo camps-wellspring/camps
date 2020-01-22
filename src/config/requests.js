282ab01d4b5d3391b37d2a4d0ddc8d5f62266a4b
Ahmed Mabrouk <a.mabrouk@solutionplus.net>
2020-01-22 17:36:17 +0200

    build for dev

--- src/config/requests.js
+++ src/config/requests.js
@@ -2,11 +2,11 @@ const base = function() {
   if (process.env.NODE_ENV === "development") {
     return "https://dev.api.adabiriyadh.sa";
   } else if (process.env.NODE_ENV === "online_dev") {
-    return "https://dev.api.solutionplus.net";
+    return "https://dev.api.adabiriyadh.sa";
   } else if (process.env.NODE_ENV === "testing") {
-    return "https://test.api.solutionplus.net";
+    return "https://test.api.adabiriyadh.sa";
   } else if (process.env.NODE_ENV === "production") {
-    return "https://api.solutionplus.net";
+    return "https://api.adabiriyadh.sa";
   }
 };
 export const baseURL = base();
